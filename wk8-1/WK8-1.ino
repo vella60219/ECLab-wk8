00c58c9b83ec62aeb574d6d1eeeec45a493e7815
vella60219 <83958906+vella60219@users.noreply.github.com>
2021-11-09 11:26:06 +0800

    Add files via upload

---
+++ wk8-1/WK8-1.ino
@@ -0,0 +1,39 @@
+#include "pitches.h"
+
+int freq = 2000;
+int channel = 0;
+int resolution = 8;
+
+int tonePin = 12;
+
+int melody[] = {
+  NOTE_A5, NOTE_C6, NOTE_D6, NOTE_D6, NOTE_E6, NOTE_E6, 
+  NOTE_E6, NOTE_G6, NOTE_A6, NOTE_D6, NOTE_C6, NOTE_E6, 
+  NOTE_A5, NOTE_C6, NOTE_D6, NOTE_D6, NOTE_E6, NOTE_E6, 
+  NOTE_E6, NOTE_F6, NOTE_E6, NOTE_D6, NOTE_C6, NOTE_C6, 
+  NOTE_A5, NOTE_C6, NOTE_D6, NOTE_D6, NOTE_E6, NOTE_E6, 
+  NOTE_E6, NOTE_G6, NOTE_A6, NOTE_D6, NOTE_C6, NOTE_E6, 
+  NOTE_A6, NOTE_C6, NOTE_F6, NOTE_E6, NOTE_D6, NOTE_C6,
+  NOTE_D6, NOTE_C6, NOTE_E6, NOTE_G6, NOTE_A6,
+};
+int noteDurations[] = {
+  1, 1, 1.5, 0.5, 1, 1,
+  1,1.5,0.5,1,1,1,
+};
+
+void setup() {
+  Serial.begin(115200);
+  ledcSetup(channel, freq, resolution);
+  ledcAttachPin(tonePin, channel);
+}
+
+void loop() {
+    for (int thisNote = 0; thisNote < 42; thisNote++) {
+      int noteDuration = 1000 / noteDurations[thisNote];
+      ledcWriteTone(channel, melody[thisNote]);
+      delay(noteDuration);
+      ledcWriteTone(channel, 0);
+      int pauseBetweenNotes = noteDuration * 1.30;
+      delay(pauseBetweenNotes);
+    }
+}
